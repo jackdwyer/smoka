312d585e623c2d53fbb404be430b6fa5a8259c08
Jack Dwyer <jackjack.dwyer@gmail.com>
2019-06-15 12:35:15 +1000

    updates to support SEEEDSTUDIO v2.1 relay shield

--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,31 @@
-int relay0 = 7; // relay pin
-int sensorPin = A0;    // select the input pin for the potentiometer
+int relay0 = 7;       // relay pin
+int sensorPin = A0;   // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
+int LEDTRIGGERED = 12;
 
-int triggered = true;         // because crap code
+int triggered = false;
 
 void setup() {
   pinMode(relay0, OUTPUT);
+  digitalWrite(LEDTRIGGERED, OUTPUT);
+
   digitalWrite(relay0, LOW);
+  digitalWrite(LEDTRIGGERED, LOW);
 }
 
 void loop() {
   sensorValue = analogRead(sensorPin);
   if (sensorValue > 800) {
-    triggered = false;
-  } else {
     triggered = true;
+  } else {
+    triggered = false;
   }
   if (triggered) {
     digitalWrite(relay0, HIGH);
+    digitalWrite(LEDTRIGGERED, HIGH);
   } else {
     digitalWrite(relay0, LOW);
+    digitalWrite(LEDTRIGGERED, LOW);
   }
   delay(5);
 }
